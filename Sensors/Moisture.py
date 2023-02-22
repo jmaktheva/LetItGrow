fc651a43087a4a8d74075df683241cceedbe5260
Harsh ajwani <harshajwani@pal-nat186-138-67.itap.purdue.edu>
2023-02-22 14:03:33 -0500

    Moisture.py updated after testing

--- Sensors/Moisture.py
+++ Sensors/Moisture.py
@@ -1,16 +1,17 @@
 import time
 import board
-from adafruit_seesaw.seesaw import seesaw
+import busio
+from adafruit_seesaw.seesaw import Seesaw
 
-i2c_bus = board.I2C()
-ss = seesaw(i2c_bus, addr = 0x36)
+i2c_bus = busio.I2C(board.IO5, board.IO4)
+ss = Seesaw(i2c_bus, addr = 0x36)
 
 while True:
-    #read moisture level
+    #read moisture level in Raw Capacitance Units
     moist = ss.moisture_read()
     print("Moisture: " + str(moist))
 
-    #read temperature
+    #read temperature in C
     temp = ss.get_temp()
     print("Temperature: " + str(temp))
 
